1926e8519bc13b72d5bf8e51d572aae22e25fec2
MdShijanAli <shijan135@gmail.com>
2026-04-06 23:39:22 +0600

    feat: remove unnecessary CORS preflight handling

--- src/app.ts
+++ src/app.ts
@@ -43,8 +43,6 @@ app.use(
   }),
 );
 
-app.options("*", cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
