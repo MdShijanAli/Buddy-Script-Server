e889edc0a25555e675dea2829d524539127e9bf2
Md Shijan Ali <shijan135@gmail.com>
2026-07-15 12:21:00 +0600

    Update: COnfiguration Update

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
+import { envVars } from "./src/config/env";
 
 export default defineConfig({
   schema: "prisma/schema",
@@ -10,7 +11,7 @@ export default defineConfig({
     // Allow Prisma generate to run in build environments where DATABASE_URL
     // is not injected at install time (for example, Vercel install step).
     url:
-      process.env.DATABASE_URL ||
+      envVars.DATABASE_URL ||
       "postgresql://placeholder:placeholder@localhost:5432/placeholder",
   },
 });
